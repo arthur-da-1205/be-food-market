b7b79431d17d41ed9bfc5d4af83664dcf91ac62b
arthur-da-1205 <arthur.darwanto@gmail.com>
2021-06-30 09:27:25 +0700

    API User Logout

--- app/Http/Controllers/APIUserController.php
+++ app/Http/Controllers/APIUserController.php
@@ -91,4 +91,11 @@ class APIUserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
 }
