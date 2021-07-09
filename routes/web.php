f99b35ca3e5a93e9d2cdcfbe6e92e87a5a0ce449
arthur-da-1205 <arthur.darwanto@gmail.com>
2021-07-09 14:50:29 +0700

    CRUD Food

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,7 @@ Route::prefix('dashboard')
         Route::get('/', [DashboardController::class, 'index'])
             ->name('dashboard');
         Route::resource('users', UserController::class);
+        Route::resource('food', FoodController::class);
     });
 
 Route::get('midtrans/success', [MidtransController::class, 'success']);
